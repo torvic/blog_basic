7006608f91c2746a69ade7a942ff8b68e40bff30
torvic <130111@unsaac.edu.pe>
2021-03-13 03:24:35 -0500

    se avanzo los post

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,17 +1,35 @@
 from django.shortcuts import render
+from .models import post,categoria
 
 # Create your views here.
 def home(request):
-    return render(request,'blog/index.html')
+    Post = post.objects.filter(estado=True)
+    return render(request,'blog/index.html',context={'post':Post})
 
 def generales(request):
-    return render(request,'blog/generales.html')
+    Categoria = categoria.objects.get(nombre='General')
+    Post = post.objects.filter(
+        estado=True, categoria=Categoria 
+    )
+    return render(request,'blog/generales.html',context={'post':Post})
 
 def programacion(request):
-    return render(request,'blog/programacion.html')
+    Categoria = categoria.objects.get(nombre='Programacion')
+    Post = post.objects.filter(
+        estado=True, categoria=Categoria 
+    )
+    return render(request,'blog/programacion.html',context={'post':Post})
 
 def videojuegos(request):
-    return render(request,'blog/videojuegos.html')
+    Categoria = categoria.objects.get(nombre='Videojuegos')
+    Post = post.objects.filter(
+        estado=True, categoria=Categoria 
+    )
+    return render(request,'blog/videojuegos.html',context={'post':Post})
 
 def tutoriales(request):
-    return render(request,'blog/tutoriales.html')
+    Categoria = categoria.objects.get(nombre='Tutoriales')
+    Post = post.objects.filter(
+        estado=True, categoria=Categoria 
+    )
+    return render(request,'blog/tutoriales.html',context={'post':Post})
